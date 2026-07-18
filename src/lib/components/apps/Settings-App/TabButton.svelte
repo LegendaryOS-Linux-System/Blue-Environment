aa76e5da1f88515c8e503920ee209f7423d963f1
michal92299 <chudybyczek2137@gmail.com>
2026-07-18 10:33:02 +0200

    Create TabButton.svelte

---
+++ src/lib/components/apps/Settings-App/TabButton.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let icon: any;
+  export let label: string;
+  export let isActive: boolean;
+</script>
+
+<button on:click
+  class="w-full flex items-center gap-3 p-2.5 rounded-lg text-sm font-medium transition-all duration-200 {isActive ? 'bg-blue-600 text-white shadow-lg shadow-blue-500/20 translate-x-1' : 'text-slate-400 hover:bg-white/5 hover:text-white'}">
+  <svelte:component this={icon} size={18} /> {label}
+</button>
