c27670bb12bd72bd30d51f2a1076c370920badea
michal92299 <chudybyczek2137@gmail.com>
2026-07-18 10:32:52 +0200

    Create types.ts

---
+++ src/lib/components/apps/Settings-App/types.ts
@@ -0,0 +1,5 @@
+export type SettingsTab =
+  | 'display' | 'personalization' | 'wifi' | 'bluetooth'
+  | 'power' | 'panel' | 'language' | 'nightLight'
+  | 'apps' | 'default_apps' | 'monitors' | 'printers' | 'users'
+  | 'accounts' | 'about' | 'security';
